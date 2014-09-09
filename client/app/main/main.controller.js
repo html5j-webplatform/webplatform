2c2ed5148384e268cdebdc3c2b955ee02cdc0ff6
Fumio SAGAWA <fumio.sagawa@gmail.com>
2014-09-09 14:42:31 +0900

    とりあえず版です

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -5,7 +5,64 @@ angular.module('webplatformApp')
     $scope.awesomeThings = [];
 
     $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+      $scope.awesomeThings = [
+		  {
+		  	name : 'プラットフォーム/デバイス',
+		  	info : ''
+		  },
+		  {
+		  	name : 'ライブラリ/フレームワーク',
+		  	info : ''
+		  },
+		  {
+		  	name : 'マネタイズマネタイズ / エコシステム',
+		  	info : ''
+		  },
+		  {
+		  	name : 'プラットフォームのアーキテクチャ',
+		  	info : ''
+		  },
+		  {
+		  	name : 'html5デバイスやAPI',
+		  	info : ''
+		  },
+		  {
+		  	name : 'HTML5(Web)セキュリティ',
+		  	info : ''
+		  },
+		  {
+		  	name : 'パッケージングと展開について',
+		  	info : ''
+		  },
+		  {
+		  	name : 'Web プラットデバッグ方法',
+		  	info : ''
+		  },
+		  {
+		  	name : 'パッケージングと展開について',
+		  	info : ''
+		  },
+		  {
+		  	name : 'SPA と ナビゲーション',
+		  	info : ''
+		  },
+		  {
+		  	name : 'オフラインについて',
+		  	info : ''
+		  },
+		  {
+		  	name : '通信と組込み機器の関係/P2P',
+		  	info : ''
+		  },
+		  {
+		  	name : 'ネイティブとハイブリッド',
+		  	info : ''
+		  },
+		  {
+		  	name : '外部機器連携',
+		  	info : ''
+		  }
+      ];
     });
 
   });
